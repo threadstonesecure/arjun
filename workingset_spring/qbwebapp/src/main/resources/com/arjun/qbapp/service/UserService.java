9f136756c73c4e00122c5aacc9b7217607568d11
PrateekinHub <prateekbumble@gmail.com>
2018-06-08 02:01:46 +0530

    removing old files, same have been moved to dev branch

--- workingset_spring/qbwebapp/src/main/resources/com/arjun/qbapp/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.arjun.qbapp.service;
-
-import com.arjun.qbapp.entity.UserMaster;
-
-public interface UserService {
-	
-	public UserMaster getUser(int Id);
-
-}
--- workingset_spring/qbwebapp/target/classes/com/arjun/qbapp/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.arjun.qbapp.dao;
-
-import com.arjun.qbapp.entity.UserMaster;
-
-public interface UserDAO {
-	
-	public UserMaster getUser(int Id);
-
-}
